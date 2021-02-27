d0aae420c39afb4aa4167a088b820a579752024b
Robert Bulai <therobertbulai@gmail.com>
2021-02-26 19:52:24 -0600

    Further implementation of InspectionInfo and MaintenanceReq

--- src/Maintenance/InspectionInfo.java
+++ src/Maintenance/InspectionInfo.java
@@ -1,9 +1,24 @@
 package Maintenance;
 
+import Facility.FacilityInfo;
+
+import java.util.HashMap;
+
 public class InspectionInfo {
     private int InspectionInfoID;
+    private HashMap<Integer, String> InspectionsList;
 
     public void setInspectionInfoID(int InfoID) { this.InspectionInfoID = InfoID; }
 
     public int getInspectionInfoID() { return this.InspectionInfoID; }
+
+    public void addNewInspection(int InfoID, String FacilityName) {
+        setInspectionInfoID(InfoID);
+
+        this.InspectionsList.put(InspectionInfoID, FacilityName);
+    }
+
+    public void listInspections() {
+        this.InspectionsList.forEach((k,v) -> System.out.println("Inspection ID: " + k + ", Name of facility: " + v));
+    }
 }
--- src/Maintenance/MaintenanceReq.java
+++ src/Maintenance/MaintenanceReq.java
@@ -2,25 +2,39 @@ package Maintenance;
 
 public class MaintenanceReq {
     private int RequestID;
-    private int MaintenanceCost;
+    int MaintenanceCost = 50; // Rate per hour for maintenance work done
+    private int hoursWorked; // Total hours worked per maintenance request
 
     public void setRequestID(int ID) { this.RequestID = ID; }
 
-    public void setMaintenanceCost(int cost) { this.MaintenanceCost = cost; }
+    public void setHoursWorked(int hours) { this.hoursWorked = hours; }
 
     public int getRequestID() { return this.RequestID; }
 
-    public int getMaintenanceCost() { return this.MaintenanceCost; }
+    public int getHoursWorked() { return this.hoursWorked; }
 
-    public void scheduleMaintenance() {
+    public void scheduleMaintenance(int ID, int date, String description) {
+        setRequestID(ID);
 
+        System.out.println("Maintenance request received.");
+        System.out.println("Request ID: " + ID);
+        System.out.println("Scheduled date: " + date);
+        System.out.println("Service description: " + description);
     }
 
-    public void calcMaintenanceCost() {
+    public void calcMaintenanceCost(int hours) {
+        setHoursWorked(hours);
 
+        int maintenanceCost = MaintenanceCost * hoursWorked;
+
+        System.out.println("Total for this maintenance service: " + maintenanceCost);
     }
 
-    public void performMaintenance() {
-
+    public void performMaintenance(int ID) {
+        System.out.println("Starting maintenance for Request #" + ID + ".");
+        System.out.println("\n...");
+        System.out.println("Maintenance underway...\n");
+        System.out.println("\n...");
+        System.out.println("Maintenance complete.\n");
     }
 }
