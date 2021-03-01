b0bbc50484bba7973ccd8d289b0823fb046e6748
Robert Bulai <therobertbulai@gmail.com>
2021-02-28 19:06:51 -0600

    Finished implementation of unit testing. Code should be good to go at this point!

--- src/Maintenance/MaintenanceInfo.java
+++ src/Maintenance/MaintenanceInfo.java
@@ -1,7 +1,5 @@
 package Maintenance;
 
-import Facility.FacilityInfo;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -10,6 +8,12 @@ public class MaintenanceInfo {
     private HashMap<Integer, Integer> MaintenanceRequestsList; // TODO: This needs to be reflected on the UML
     private HashMap<Integer, String> MaintenanceDescList; // TODO: This needs to be reflected on the UML
     private ArrayList<String> FacilityProblems; // TODO: This needs to be reflected on the UML
+
+    public MaintenanceInfo() {
+        this.MaintenanceRequestsList = new HashMap<>();
+        this.MaintenanceDescList = new HashMap<>();
+        this.FacilityProblems = new ArrayList<>();
+    }
 
     public void setFacilityID(int ID) { this.FacilityID = ID; } // TODO: This needs to be reflected on the UML
 
@@ -27,20 +31,22 @@ public class MaintenanceInfo {
     }
 
 
-    public void calcProblemRateForFacility() {
-        System.out.println("This facility has had " + this.FacilityProblems.size() + "problems so far.");
+    public int calcProblemRateForFacility() {
+        System.out.print("This facility has had " + this.FacilityProblems.size() + "problems so far.\n");
+
+        return FacilityProblems.size();
     }
 
     public void listMaintRequests() {
-        this.MaintenanceRequestsList.forEach((k,v) -> System.out.println("This request is for facility ID# " + k + ", and the maintenance request is ID# " + v + "."));
+        this.MaintenanceRequestsList.forEach((k,v) -> System.out.print("This request is for facility ID#" + k + ", and the maintenance request is ID#" + v + ".\n"));
     }
 
     public void listMaintenance() {
-        this.MaintenanceDescList.forEach((k,v) -> System.out.println("Request ID#" + k + " . Maintenance description: " + v));
+        this.MaintenanceDescList.forEach((k,v) -> System.out.print("Request ID#" + k + " . Maintenance description: " + v + "\n"));
     }
 
     public void listFacilityProblems() {
         for (String problem : FacilityProblems)
-            System.out.println(problem);
+            System.out.print(problem + "\n");
     }
 }
